4152e7d085ff5b9db086e05577da6c503bd06052
holla394 <samuel.holland@emerson.com>
2022-10-12 21:53:30 -0500

    changed pybank main.py file to use csv reader module instead of pandas -- saved a copy of the pandas version as main_pandas.py in the same directory -- they do the same thing.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,44 +1,58 @@
-import pandas as pd
+from itertools import count
+import os
+from csv import reader
 
-count = 0
+months = 0
 net = 0
 biggest = 0
 smallest = 0
 changes = []
 
-data = pd.read_csv("~\Bootcamp\challenges\python-challenge\PyBank\Resources\Budget_Data.csv",',',None,0)
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+csvpath = os.path.join('Resources','budget_data.csv')
 
-for index,series in data.iterrows():
-    # show output of data series:
-    # print(series.name)
-    # print(series.values[0])
-    # print(series.values[1])
-    
-    # calculating sum
-    net += series.values[1]
+with open(csvpath) as csvfile:
+    csvreader = reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
 
-    # checking for and assigning biggest change
-    if series.values[1] > biggest:
-        biggest = series.values[1]
-        biggest_date = series.values[0]
-    
-    # checking for and assigning smallest change
-    if series.values[1] < smallest:
-        smallest = series.values[1]
-        smallest_date = series.values[0]
+    # iterate through rows in csvfile
+    for row in csvreader:
+        
+        # adding change in profits/losses if not the first row
+        if months != 0:
+            changes.append(int(row[1]) - last_change)
+        
+        # checking for and assigning biggest change
+        if int(row[1]) > biggest:
+            biggest = int(row[1])
+            biggest_date = row[0]
 
-    # adding change in profits/losses if not the first row
-    if index != 0:
-        changes.append(series.values[1] - last_change)
+        # checking for and assigning smallest change
+        if int(row[1]) < smallest:
+            smallest = int(row[1])
+            smallest_date = row[0]
 
-    # saving last change for above calculation
-    last_change = series.values[1]
+        # saving last change for above calculation
+        last_change = int(row[1])
+
+        # adding a month
+        months += 1
+
+        # calculating sum
+        net += int(row[1])
 
 # output to txt file
 with open('analysis\data.txt','w') as f:
     f.write('Financial Analysis \n----------------------------\n')
-    f.write(f'Total Months: {len(data)}\n')
+    f.write(f'Total Months: {months}\n')
     f.write(f'Total: ${net}\n')
     f.write(f'Average change: ${round(sum(changes)/len(changes),2)}\n')
     f.write(f'Greatest Increase in Profits: {biggest_date} (${biggest})\n')
     f.write(f'Greatest Decrease in Profits: {smallest_date} (${smallest})\n')
+
+print('\n\nFinancial Analysis \n----------------------------')
+print(f'Total Months: {months}')
+print(f'Total: ${net}')
+print(f'Average change: ${round(sum(changes)/len(changes),2)}')
+print(f'Greatest Increase in Profits: {biggest_date} (${biggest})')
+print(f'Greatest Decrease in Profits: {smallest_date} (${smallest})\n')
